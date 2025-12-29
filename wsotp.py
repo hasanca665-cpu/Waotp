e441fd7e2ee59ec2742dc448b405cc06ace95097
hasanca665-cpu <hasanca665@gmail.com>
2025-12-29 02:21:53 -0600

    Update wsotp.py

--- wsotp.py
+++ wsotp.py
@@ -102,7 +102,7 @@ async def keep_alive_enhanced():
     """Enhanced keep-alive with multiple strategies for Render"""
     keep_alive_urls = [
         "https://waotp-dpqw.onrender.com",
-        "https://autoping-6hvq.onrender.com"
+        "https://autoping-1-ymen.onrender.com"
     ]
     
     while True:
